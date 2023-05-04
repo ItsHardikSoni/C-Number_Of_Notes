672689ae57e144d47f49ef17b9d41a7705003a8c
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-04 11:55:10 +0530

    Update and rename Number of Notes Using (%) Modular Operators to Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,8 @@
-/* Number of Notes Using (%) Modular Operator */
+// Consider a currency system in which there are notes of six denominations,
+// namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If a sum of Rs. N is entered through the keyboard,
+// write a program to compute the smallest number of notes that will combine to give Rs. N.
+
+
 #include<stdio.h>
 #include<conio.h>
 int main()
